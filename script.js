191fcaf924bc232e9ab9d72686862cb01ac0ebfe
Salypse <84153483+Salypse@users.noreply.github.com>
2025-08-27 11:23:08 -0500

    Create basic calculator functions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World!")
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+function operate(num1, operand, num2) {
+    switch(operand) {
+        case "+" :
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default :
+            return "Invalid Operator"
+    }
+}
